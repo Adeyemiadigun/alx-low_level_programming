ca2a84affa88e11d12293b4bff8db972183efc98
Adeyemi Mubarak <adeyemiadigun12@gmail.com>
2023-02-16 16:04:11 -0800

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
 
 int main(void)
 {
-	printf(stderr, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n");
+	fprintf("and that piece of art is useful\" - Dora Korper, 2015-10-19\n");
 	return (1);
 }
